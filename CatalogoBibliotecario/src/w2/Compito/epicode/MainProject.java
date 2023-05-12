88b72a03fcb8dc246498a1d7aebb610f71f8868d
Cujba Tiberiu Stefan <123594218+CujbaTiberiu@users.noreply.github.com>
2023-05-12 15:13:05 +0200

    wip...

--- CatalogoBibliotecario/src/w2/Compito/epicode/MainProject.java
+++ CatalogoBibliotecario/src/w2/Compito/epicode/MainProject.java
@@ -22,6 +22,7 @@ public class MainProject {
 
 	public static void main(String[] args) {
 		
+		//log.info("--- Elementi aggiunti ---");
 		catalogo.add(new Libro("1L", "Il trono di spade", 2015, 400,"George R.R. Martin" ,"Fantasy"));
 		catalogo.add(new Libro("2L", "Arte della guerra", 100, 600,"Sun Tzu" ,"Non-Fiction"));
 		catalogo.add(new Libro("3L", "Il signore degli Anelli", 2015, 400,"J. R. R. Tolkien" ,"Fantasy"));
@@ -30,6 +31,7 @@ public class MainProject {
 		catalogo.add(new Rivista("2R", "TechnologyTomorrow", 2002, 100, Periodicità.MENSILE));
 		catalogo.add(new Rivista("3R", "MotorShow", 2006, 150, Periodicità.SEMESTRALE));
 		
+		  log.info("--- Elenco catalogo ---");
 	    catalogo.forEach(e -> System.out.println(e));
 	    //try {
 			//scriviSuFile();
@@ -37,8 +39,25 @@ public class MainProject {
 			//log.info("Scritto su file!");
 			//e1.printStackTrace();
 		//}
+	    log.info("--- Rimozione di un elemento ---");
 	    String elementoRimosso = removeElement("3L");
-	    System.out.println("Elemento rimosso" + elementoRimosso);
+	    log.info("Elemento rimosso" + elementoRimosso);
+	    
+	    log.info("--- Elenco catalogo ---");
+	    catalogo.forEach(e -> System.out.println(e));
+	    
+	    log.info("--- Ricerca di un elemento per ISBN ---");
+	    String ricercaPerIsbn = findByIsbn("3R").toString();
+	    log.info("Elemento ricercato per codice ISBN" + ricercaPerIsbn);
+	    
+	    log.info("--- Ricerca di un elemento per Anno pubblicazione ---");
+	    String ricercaPerAnnoPub = findByPubYear(2015).toString();
+	    log.info("Elemento ricercato per anno pubblicazione" + ricercaPerAnnoPub);
+	    
+	    log.info("--- Ricerca di un elemento per Autore ---");
+	    String ricercaPerAutore = findByAuthor("Sun Tzu").toString();
+	    log.info("Elemento ricercato per Autore" + ricercaPerAutore);
+	    
 	}
 	
 	public static String removeElement(String ISBN) {
@@ -53,6 +72,18 @@ public class MainProject {
 			return null;
 		}
 		
+	}
+	
+	public static List<Catalogo> findByIsbn(String ISBN) {
+		return catalogo.stream().filter(e -> e.getCodiceISBN().equals(ISBN)).collect(Collectors.toList());
+	}
+	
+	public static List<Catalogo> findByPubYear(int year) {
+		return catalogo.stream().filter(e -> e.getAnnoPublicazione() == (year)).collect(Collectors.toList());
+	}
+	
+	public static List<Catalogo> findByAuthor(String autore) {
+		return catalogo.stream().filter(e -> e instanceof Libro && ((Libro)e).getAutore().equals(autore)).collect(Collectors.toList());
 	}
 	
 	public static void scriviSuFile() throws IOException {
