65928400d3be76bcb2d7e79a23d976a5ccc7b09c
Cujba Tiberiu Stefan <123594218+CujbaTiberiu@users.noreply.github.com>
2023-06-11 00:19:18 +0200

    web app working well

--- GestioneDispositiviAziendali/src/main/java/com/compito/GestioneDispositiviAziendali/config/SecurityConfig.java
+++ GestioneDispositiviAziendali/src/main/java/com/compito/GestioneDispositiviAziendali/config/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.compito.GestioneDispositiviAziendali.security.JwtAuthenticationEntryPoint;
 import com.compito.GestioneDispositiviAziendali.security.JwtAuthenticationFilter;
@@ -66,4 +68,18 @@ public class SecurityConfig {
     	return http.build();
     }
 
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000") 
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
